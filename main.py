ff37eb984d6c0593f696e1071385fcd66ebc516b
René Nagelhout <rfwnag@hotmail.com>
2023-09-30 10:52:28 +0200

    Final Tests

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 # Assign a function to be called when the path '/' is requested
 @app.route('/')
 def index():
-        return 'Hello, world!'
+        return 'Hello, WINC!!'
 
 @app.route('/cow')
 def cow():
-        return 'MOooorfhftwsf eesvefthrsfso!'
+        return 'MOsldkfv idsf!'
 
 if __name__ ==  "__main__":
         app.run(debug=True)
